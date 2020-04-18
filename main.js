6b1cac429b53f76fc31c0464fee4f6f2bf889acb
Steven Rutherford <code@steven-rutherford.com>
2020-04-18 13:36:29 +1200

    Added tree, platform and character

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-var GAME_SCREEN_WIDTH = 800;
-var GAME_SCREEN_HEIGHT = 600;
+var GAME_SCREEN_WIDTH = 900;
+var GAME_SCREEN_HEIGHT = 400;
 
 Crafty.init(GAME_SCREEN_WIDTH,GAME_SCREEN_HEIGHT, document.getElementById('game'));
 
--- scenes/game.js
+++ scenes/game.js
@@ -1,7 +1,50 @@
-Crafty.defineScene("Game", function() {
-	
-	var redSquare = Crafty.e('2D, DOM, Color')
-		.attr({x: 0, y: 0, w: 100, h: 100})
-		.color('#F00');
+var PLATFORM_HEIGHT = 30;
 
+Crafty.defineScene("Game", function() {
+	var floor = Crafty.e("2D, DOM, Color, Platform")
+		.attr({
+			x: -900, 
+			y: GAME_SCREEN_HEIGHT - PLATFORM_HEIGHT, 
+			w: 2700, 
+			h: PLATFORM_HEIGHT
+		})
+		.color("#563");
+
+	var platform1 = Crafty.e("2D, DOM, Color, Platform")
+		.attr({
+			x: 150, 
+			y: GAME_SCREEN_HEIGHT - PLATFORM_HEIGHT * 5, 
+			w: 150, 
+			h: PLATFORM_HEIGHT
+		})
+		.color("#563");
+
+	var platform2 = Crafty.e("2D, DOM, Color, Platform")
+		.attr({
+			x: 650, 
+			y: GAME_SCREEN_HEIGHT - PLATFORM_HEIGHT * 5, 
+			w: 150, 
+			h: PLATFORM_HEIGHT
+		})
+		.color("#563");
+
+	var tree = Crafty.e("2D, DOM, Color, Platform")
+		.attr({
+			x: 400, 
+			y: GAME_SCREEN_HEIGHT - PLATFORM_HEIGHT * 6,
+			w: PLATFORM_HEIGHT * 5,
+			h: PLATFORM_HEIGHT * 5
+		})
+		.color("#382");
+
+	var player = Crafty.e("2D, DOM, Color, Twoway, Gravity")
+		.attr({
+			x: 50, 
+			y: GAME_SCREEN_HEIGHT - PLATFORM_HEIGHT * 6,
+			w: 35,
+			h: 70
+		})
+		.gravity("Platform")
+		.twoway(200, 350)
+		.color("#832");
 });
