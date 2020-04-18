e4e170e9695cae0e7eb435f9952d11511b2f467e
Steven Rutherford <code@steven-rutherford.com>
2020-04-18 17:43:12 +1200

    Spread platfomrs out

--- scenes/game.js
+++ scenes/game.js
@@ -13,13 +13,13 @@ Crafty.defineScene("Game", function() {
 
 	var bucketSpawner = Crafty.e("BucketSpawner");
 
-	var platform1 = Crafty.e("Platform").place(200, GAME_SCREEN_HEIGHT - PLATFORM_HEIGHT * 5);
+	var platform1 = Crafty.e("Platform").place(100, GAME_SCREEN_HEIGHT - PLATFORM_HEIGHT * 4);
 
-	var platform2 = Crafty.e("Platform").place(700, GAME_SCREEN_HEIGHT - PLATFORM_HEIGHT * 5);
+	var platform2 = Crafty.e("Platform").place(900, GAME_SCREEN_HEIGHT - PLATFORM_HEIGHT * 4);
 
-	var platform2 = Crafty.e("Platform").place(300, GAME_SCREEN_HEIGHT - PLATFORM_HEIGHT * 11);
+	var platform2 = Crafty.e("Platform").place(250, GAME_SCREEN_HEIGHT - PLATFORM_HEIGHT * 10);
 
-	var platform2 = Crafty.e("Platform").place(600, GAME_SCREEN_HEIGHT - PLATFORM_HEIGHT * 11);
+	var platform2 = Crafty.e("Platform").place(700, GAME_SCREEN_HEIGHT - PLATFORM_HEIGHT * 10);
 
 	var tree = Crafty.e("Tree").place(400, GAME_SCREEN_HEIGHT - ((989 / 4)) - 10);
 
