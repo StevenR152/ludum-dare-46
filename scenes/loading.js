1d39b1d95ce80c479bca644c925584308db4a9e8
Steven Rutherford <code@steven-rutherford.com>
2020-04-19 00:01:13 +1200

    switch loading screen to go directly to game screen

--- scenes/loading.js
+++ scenes/loading.js
@@ -12,7 +12,7 @@ Crafty.defineScene("Loading", function() {
 
   	Crafty.load(gameAssets, function(){
        setTimeout(function () {
-         Crafty.scene('EndScreen');
+         Crafty.scene('Game');
        }, 1000);
     },  function () {
       console.log("Crafty Load Issue");
