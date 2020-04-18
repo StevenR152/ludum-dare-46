f31f771429061d96a36bdf30d33ba81755492448
Josh Pearson <joshuapearsonnz@gmail.com>
2020-04-18 13:57:53 +1200

    add bucket component

--- components/bucket.js
+++ components/bucket.js
@@ -1,9 +1,9 @@
 Crafty.c("Bucket", {
 	init: function() {
         this.addComponent("2D, Canvas, Color");
-        this.y = GAME_SCREEN_HEIGHT - PLATFORM_HEIGHT * 3;
-        this.h = PLATFORM_HEIGHT;
-        this.color("#563");
+        this.w = 30;
+        this.h = 30;
+        this.color("#569");
     },
 
     place: function(x, y) {
--- scenes/game.js
+++ scenes/game.js
@@ -3,8 +3,8 @@ var PLATFORM_HEIGHT = 30;
 Crafty.defineScene("Game", function() {
 	var floor = Crafty.e("Platform").place(150, GAME_SCREEN_HEIGHT - PLATFORM_HEIGHT * 5)
 		.attr({
-			x: -900, 
-			y: GAME_SCREEN_HEIGHT - PLATFORM_HEIGHT, 
+			x: -900,
+			y: GAME_SCREEN_HEIGHT - PLATFORM_HEIGHT,
 			w: 2700
 		});
 
@@ -14,12 +14,14 @@ Crafty.defineScene("Game", function() {
 
 	var tree = Crafty.e("2D, DOM, Color, Tree")
 		.attr({
-			x: 400, 
+			x: 400,
 			y: GAME_SCREEN_HEIGHT - PLATFORM_HEIGHT * 6,
 			w: PLATFORM_HEIGHT * 5,
 			h: PLATFORM_HEIGHT * 5
 		})
 		.color("#382");
+
+	var bucket1 = Crafty.e("Bucket").place(700,200);
 
 	var player = Crafty.e("Player").place(50, GAME_SCREEN_HEIGHT - PLATFORM_HEIGHT * 5);
 });
