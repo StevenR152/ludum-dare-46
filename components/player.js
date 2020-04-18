f5897f301c8a2f5a9f7d34821162e740a1028971
Josh Pearson <joshuapearsonnz@gmail.com>
2020-04-18 14:13:26 +1200

    added collision to player/bucket

--- components/player.js
+++ components/player.js
@@ -1,15 +1,15 @@
 Crafty.c("Player", {
 	init: function() {
-        this.addComponent("2D, DOM, Color, Twoway, Gravity");
+        this.addComponent("2D, DOM, Color, Twoway, Gravity, Collision");
         this.x = 50;
         this.y = GAME_SCREEN_HEIGHT - PLATFORM_HEIGHT * 6;
         this.w = 35;
         this.h = 70;
-		this.gravity("Platform")
-		this.twoway(200, 350)
+		this.gravity("Platform");
+		this.twoway(200, 350);
 		this.color("#832");
+		this.onHit("Bucket", function() {});
     },
-
     place: function(x, y) {
         this.x = x;
         this.y = y;
