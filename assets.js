6060bbe01f589be0cbcde3c8e9b85d94d143c0ed
Steven Rutherford <code@steven-rutherford.com>
2020-04-18 15:05:23 +1200

    images of bear, tree and bucket!

--- assets.js
+++ assets.js
@@ -14,12 +14,12 @@ var gameAssets = {
         "assets/images/bucket.png": {
             "tile": 500,
             "tileh": 500,
-            "map": {"player": [0, 0]}
+            "map": {"bucket": [0, 0]}
         },
         "assets/images/Tree.png": {
             "tile": 1000,
             "tileh": 1000,
-            "map": {"player": [0, 0]}
+            "map": {"tree": [0, 0]}
         },
     },
 };
