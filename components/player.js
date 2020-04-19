018c5468083ad4eb97e3f9a9f600522fff23936a
Josh Pearson <joshuapearsonnz@gmail.com>
2020-04-19 22:50:02 +1200

    added global trigger for raindrops

--- components/player.js
+++ components/player.js
@@ -66,7 +66,7 @@ Crafty.c("Player", {
 				inBucket.water += 1;
 				this.charImg();
 				collectWater[0].obj.destroy();
-				Crafty.trigger("getRaindrop");
+				Crafty.trigger("Raindrop Update");
 			}
 		})
     },
