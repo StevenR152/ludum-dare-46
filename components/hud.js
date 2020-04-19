2b46d6ad322d978f35dd695ba6f44e0b3bdaf3d7
Steven Rutherford <code@steven-rutherford.com>
2020-04-19 21:12:14 +1200

    added HUD component to wrap hud elements

--- components/hud.js
+++ components/hud.js
@@ -1,15 +1,22 @@
 Crafty.c("HUD", {
     init: function() { 
-    	this.bind("EnterFrame", this.adjustPosition) 
-    },
+        this.addComponent("2D, DOM, Color");
+        this.origin("center")
+        this.z = 500;
+        this.x = -438;
+        this.w = GAME_SCREEN_WIDTH + 140;
+        this.h = 50;
+        this.y = GAME_SCREEN_HEIGHT - 30;
+        this.color("white")
+        this.alpha = 0.08;
 
-    fixedPosition: function(x, y) {
-         this.fixedX = x; this.fixedY=y;
-         return this;
-    },
 
-    adjustPosition: function() {
-          this.x = this.fixedX - Crafty.viewport.x;
-          this.y = this.fixedY - Crafty.viewport.y;
-    }
+		var hudCounter = Crafty.e("HudCounter").attr({x:515, y: GAME_SCREEN_HEIGHT - 20});
+		var hudTreeLife = Crafty.e("HudHealth")
+	
+		this.attach(hudTreeLife);
+		this.attach(hudCounter);
+
+		hudTreeLife.attr({x:-440 + 20, y: GAME_SCREEN_HEIGHT - 20});
+    },
 })
