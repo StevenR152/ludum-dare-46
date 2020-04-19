47edc98b25b14fdfb4bf8df1e595b0403bbd122c
Josh Pearson <joshuapearsonnz@gmail.com>
2020-04-19 15:07:34 +1200

    removed bucket spawning, character stays with one bucket only

--- components/bucket_spawner.js
+++ components/bucket_spawner.js
@@ -1,8 +1,8 @@
 Crafty.c("BucketSpawner", {
 	init: function() {
         this.addComponent("BucketSpawner, 2D, Delay");
-		this.bind("waterTree", function() {
-			this.delay(this.spawnNewBucket, 1000, 0);
+		// this.bind("waterTree", function() {
+		// 	this.delay(this.spawnNewBucket, 1000, 0);
 		}); // called in player component when collission with tree occurs
     },
 	spawnNewBucket: function() {
