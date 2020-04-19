0c6755b569248b234e61e6b7982e05c55a10542f
Steven Rutherford <code@steven-rutherford.com>
2020-04-19 17:17:18 +1200

    viewport scaled

--- cameratrack.js
+++ cameratrack.js
@@ -1,7 +1,7 @@
 function makeCameraTrackEntity(entity, yoffset) {
     // Sets up the Camera Tracking to the player entity
     Crafty.viewport.clampToEntities = false;
-    Crafty.viewport.scale(1);
+    Crafty.viewport.scale(0.85);
     Crafty.one("CameraAnimationDone", function () {
         Crafty.viewport.follow(entity, 0, 0 + yoffset);
     });
