6dff2be1b130438ae50079b3582bf1838b1e7b16
Josh Pearson <joshuapearsonnz@gmail.com>
2020-04-19 12:06:04 +1200

    dead tree img added and working raindrops fall

--- components/raindrop.js
+++ components/raindrop.js
@@ -1,16 +1,14 @@
 Crafty.c("Raindrop", {
 	init: function() {
-        this.addComponent("2D, Collision, Motion, Color, DOM, Raindrop");
-        this.w = 71 / 10;
-        this.h = 98 / 10;
+        this.addComponent("2D, Collision, Motion, DOM, raindrop");
+        this.w = 71 / 6;
+        this.h = 98 / 6;
         this.z = 8;
         this.vy = 250;
 		//this.color("#0000FF");
 		// this.gravity("Ground");
-		console.log("rain");
 		this.onHit("solid", function() {
 			Crafty.trigger("raindropMissed");
-			console.log("rain die");
 			this.destroy();
 		});
     },
