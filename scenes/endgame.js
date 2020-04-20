3adf17b5731324a9e229648aa51e5d91b48b8276
Steven Rutherford <code@steven-rutherford.com>
2020-04-20 19:27:27 +1200

    load all assets from assets file

--- scenes/endgame.js
+++ scenes/endgame.js
@@ -1,9 +1,8 @@
 Crafty.defineScene("EndScreen", function() {
 	var gameOverBackground = Crafty.e("EndBackground");
 
-	var playAgainGameButton = Crafty.e("2D, DOM, Image, Mouse, playagain")
+	var playAgainGameButton = Crafty.e("2D, DOM, Mouse, restart")
 		.attr({x: GAME_SCREEN_WIDTH-250, y: GAME_SCREEN_HEIGHT-150})
-		.image("assets/images/restart.png")
 		.bind('Click', function(MouseEvent){
         	Crafty.scene('Game');
 		});
