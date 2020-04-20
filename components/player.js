45eed5f66379c27f84d887f3346be82bd0c65716
Steven Rutherford <code@steven-rutherford.com>
2020-04-20 13:27:42 +1200

    refactored player

--- components/player.js
+++ components/player.js
@@ -16,26 +16,9 @@ Crafty.c("Player", {
         ]);
 
 		this.twoway(player_speed, player_jump);
-
-		this.onHit("Bucket", function(collectBucket) {
-			collectBucket[0].obj.destroy();
-			var raindropSpawner = Crafty.e("RaindropSpawner");
-			hasBucket = true;
-			this.charImg();
-			Crafty.trigger("collectBucket");
-		});
-
-		this.onHit("Poo", function(collectPoo) {
-			if (inBucket.poo < 3) {
-				collectPoo[0].obj.destroy();
-				inBucket.poo += 1;
-				this.charImg();
-				Crafty.trigger("getPoo");
-			}
-			else {
-				Crafty.trigger("fullBucket");
-			}
-		});
+		
+		this.setKeybinding();
+		this.setCollisionHitActions();
 
 		this.bind("CheckLanding", function(ground) {
 			// disallow landing of player, if player's feet are not above ground
@@ -46,56 +29,7 @@ Crafty.c("Player", {
 
 		this.bind("changePlayerSpeed", function() {
 			this.twoway(player_speed, player_jump);
-		});
-
-		this.bind("KeyDown", function(arrow) {
-			if (arrow.key == Crafty.keys.LEFT_ARROW) {
-				facing = "left";
-				this.charImg();
-			} else if (arrow.key == Crafty.keys.RIGHT_ARROW) {
-				facing = "right";
-				this.charImg();
-			}
-		});
-
-		this.bind("KeyDown", function(powerup) {
-			if (powerup.key == Crafty.keys["1"]) {
-				Crafty.trigger("powerupSpeed");
-			}
-			else if (powerup.key == Crafty.keys["2"]) {
-				Crafty.trigger("powerupJump");
-				this.twoway(player_speed, player_jump);
-			}
-			else if (powerup.key == Crafty.keys["3"]) {
-				Crafty.trigger("powerupRain");
-			}
-		});
-
-		this.bind("KeyDown", function(debugPlayer) {
-			if (debugPlayer.key == Crafty.keys.SPACE) {
-				console.log((this.x + (this.w/4)), + " " + (this.y + (this.h/1.2)));
-			} // debug code used for player positioning
-		});
-
-		this.onHit("Tree", function(emptyBucket) {;
-			if (inBucket.water > 0 || inBucket.poo > 0) {
-				Crafty.trigger("emptyBucket");
-				this.charImg();
-				healing_strength = DEFAULT_HEALING_STR; // RESET healing strength
-			}
-		});
-
-		this.onHit("Raindrop", function(collectWater) {
-			if (inBucket.water < 5) {
-				inBucket.water += 1;
-				this.charImg();
-				collectWater[0].obj.destroy();
-				Crafty.trigger("getRaindrop");
-			}
-			else {
-				Crafty.trigger("fullBucket");
-			}
-		})
+		});		
     },
     place: function(x, y) {
         this.x = x;
@@ -146,5 +80,77 @@ Crafty.c("Player", {
 				this.h = 429 / 5;
 			}
 		}
+	},
+	setKeybinding: function () {
+		this.bind("KeyDown", function(arrow) {
+			if (arrow.key == Crafty.keys.LEFT_ARROW) {
+				facing = "left";
+				this.charImg();
+			} else if (arrow.key == Crafty.keys.RIGHT_ARROW) {
+				facing = "right";
+				this.charImg();
+			}
+		});
+
+		this.bind("KeyDown", function(powerup) {
+			if (powerup.key == Crafty.keys["1"]) {
+				Crafty.trigger("powerupSpeed");
+			}
+			else if (powerup.key == Crafty.keys["2"]) {
+				Crafty.trigger("powerupJump");
+				this.twoway(player_speed, player_jump);
+			}
+			else if (powerup.key == Crafty.keys["3"]) {
+				Crafty.trigger("powerupRain");
+			}
+		});
+
+		this.bind("KeyDown", function(debugPlayer) {
+			if (debugPlayer.key == Crafty.keys.SPACE) {
+				console.log((this.x + (this.w/4)), + " " + (this.y + (this.h/1.2)));
+			} // debug code used for player positioning
+		});
+	},
+	setCollisionHitActions: function () {
+		this.onHit("Bucket", function(collectBucket) {
+			collectBucket[0].obj.destroy();
+			var raindropSpawner = Crafty.e("RaindropSpawner");
+			hasBucket = true;
+			this.charImg();
+			Crafty.trigger("collectBucket");
+		});
+
+		this.onHit("Poo", function(collectPoo) {
+			if (inBucket.poo < 3) {
+				collectPoo[0].obj.destroy();
+				inBucket.poo += 1;
+				this.charImg();
+				Crafty.trigger("getPoo");
+			}
+			else {
+				Crafty.trigger("fullBucket");
+			}
+		});
+
+
+		this.onHit("Tree", function(emptyBucket) {;
+			if (inBucket.water > 0 || inBucket.poo > 0) {
+				Crafty.trigger("emptyBucket");
+				this.charImg();
+				healing_strength = DEFAULT_HEALING_STR; // RESET healing strength
+			}
+		});
+
+		this.onHit("Raindrop", function(collectWater) {
+			if (inBucket.water < 5) {
+				inBucket.water += 1;
+				this.charImg();
+				collectWater[0].obj.destroy();
+				Crafty.trigger("getRaindrop");
+			}
+			else {
+				Crafty.trigger("fullBucket");
+			}
+		})
 	}
 })
