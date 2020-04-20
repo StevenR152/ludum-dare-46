7eb7970b04636dba753d8a805c38b62d97c7cf17
Steven Rutherford <code@steven-rutherford.com>
2020-04-21 01:19:11 +1200

    fixed homescreen alignment

--- scenes/homescreen.js
+++ scenes/homescreen.js
@@ -2,11 +2,11 @@ Crafty.defineScene("HomeScreen", function() {
 	var gameStartBackground = Crafty.e("StartBackground");
 
 	var playGameButton = Crafty.e("2D, DOM, Image, Mouse, playbutton")
-		.attr({x: GAME_SCREEN_WIDTH-250, y: GAME_SCREEN_HEIGHT-150})
+		.attr({x: GAME_SCREEN_WIDTH-200, y: GAME_SCREEN_HEIGHT-140})
 		.bind('Click', function(MouseEvent){
         	Crafty.scene('Game');
 		});
 
 	var title = Crafty.e("2D, DOM, title")
-		.attr({x: 150, y: 0})
+		.attr({x: 260, y: 0})
 });
